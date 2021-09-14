6009768c64af60d73e74b744a167ebd591202907
Sarinho01 <alanvictor.sarinho@gmail.com>
2021-09-14 10:16:00 -0300

    Teste1.0

---
+++ src/Teste.java
@@ -0,0 +1,13 @@
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Teste do alan chad");
+        int[] teste = {1,2,3};
+
+        for (int x : teste) {
+            System.out.println(x);
+        }
+
+
+        System.out.println(teste[1]);
+    }
+}
