edbe415cdd02815d269419dc6abf718f12139b7b
Sarinho01 <alanvictor.sarinho@gmail.com>
2021-09-30 17:05:13 -0300

    Colocado na pasta

--- src/me/testeGit/Teste.java
+++ src/me/testeGit/Teste.java
@@ -1,6 +1,8 @@
+package me.testeGit;
+
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("Teste do alan chad");
+        System.out.println("me.testeGit.Teste do alan chad");
         int[] teste = {1,2,3};
 
         for (int x : teste) {
